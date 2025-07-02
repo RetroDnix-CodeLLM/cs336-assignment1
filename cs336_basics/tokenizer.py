9978e46351ec39a2dfd6408e6b5cd75ffd4f6870
RetroDnix <retrodnix@gmail.com>
2025-07-02 16:52:45 +0800

    feat: BPE can't pass speed test

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -1,13 +1,14 @@
 import os
 import regex as re
 from tqdm import tqdm
-from pprint import pprint
+
+from .utils import UnionFindSet, increaseD, decreaseD, appendD, removeD
 
 class BPETokenizer():
     def __init__(self, corpus:str, special_tokens: list[str] = ["<|endoftext|>"]):
         self.vocab = {}
         for i in range(256):
-            self.vocab[chr(i).encode("utf-8")] = i
+            self.vocab[bytes([i,])] = i
         
         for sp_token in special_tokens:
             self.vocab[sp_token.encode("utf-8")] = len(self.vocab)
@@ -51,57 +52,79 @@ class BPETokenizer():
             maximum_vocab_size (int): 训练BPE的最大词汇表大小，默认为10000
 
         """
-        appearances = {}
-        prefix = {}
-        suffix = {}
+        appearances = {}    # apprearance[pair]: 表示字符对pair出现的频率
+        position = {}       # position[pair]: 表示字符对pair(a,b)出现的位置（使用b的首字节作为标记）
+        value = {}          # value[(i,j,k)]: 表示位置(i,j,k)的字节对的值（首字节标记）
+        ufs = {}
         
         print("Training BPE tokenizer...")
         print("Pre-calculating appearances of byte pairs...")
         maxn = 0
         maxp = None
         if len(self.vocab) < maximum_vocab_size:
-            for words in self.pretokenized_corpus:
-                for word in words:
+            for i, words in enumerate(self.pretokenized_corpus):
+                for j, word in enumerate(words):
+                    # i, j 是语料库中第i行的第j个预分词token
                     bs = [(b, ) for b in word.encode("utf-8")]
-                    for i in range(len(bs) - 1):
-                        pair = (bs[i], bs[i + 1])
-                        if pair not in prefix:
-                            prefix[pair] = []
-                        if pair not in suffix:
-                            suffix[pair] = []
-                        if i > 0:
-                            prefix[pair].append(bs[i - 1])
-                        if i < len(bs) - 2:
-                            suffix[pair].append(bs[i + 2])
-                        appearances[pair] = appearances.get(pair, 0) + 1
-                        if appearances[pair] >= maxn:
-                            maxn = appearances[pair]
-                            maxp = pair
-        
+                    l = len(bs)
+                    s = ufs[(i, j)] = UnionFindSet(size = l)
+                    v = value[(i, j)] = {}
+                    for k in range(l):
+                        v[k] = bs[k]
+
+                        if k < l - 1:
+                            pair = (bs[k], bs[k + 1])
+                            increaseD(appearances, pair)
+                            appendD(position, pair, (i, j, k))
+                            if appearances[pair] > maxn or (appearances[pair] == maxn and pair > maxp):
+                                maxn = appearances[pair]
+                                maxp = pair
+            
         pbar = tqdm(total=maximum_vocab_size - len(self.vocab), desc="Merging byte pairs")
         while len(self.vocab) < maximum_vocab_size:
             current_vocab_size = len(self.vocab)
             if maxp is not None:
-                self.merges.append(maxp)
-                appearances[maxp] = 0
-                maxpb = maxp[0] + maxp[1]
-                self.vocab[bytes(maxpb)] = len(self.vocab)
+                bMaxPair = maxp[0] + maxp[1]
+                self.merges.append((bytes(maxp[0]), bytes(maxp[1])))
+                self.vocab[bytes(bMaxPair)] = len(self.vocab)
+
                 pbar.update(len(self.vocab) - current_vocab_size) 
                 
-                for pre_b in prefix.get(maxp, []):
-                    appearances[(pre_b, maxp[0])] = appearances.get((pre_b, maxp[0]), 0) - 1
-                    appearances[(pre_b, maxpb)] = appearances.get((pre_b, maxpb), 0) + 1
-                    prefix[(pre_b, maxpb)] = prefix.get((pre_b, maxp[0]), [])
-                
-                for suf_b in suffix.get(maxp, []):
-                    appearances[(maxp[1], suf_b)] = appearances.get((maxp[1], suf_b), 0) - 1
-                    appearances[(maxpb, suf_b)] = appearances.get((maxpb, suf_b), 0) + 1
-                    suffix[(maxpb, suf_b)] = suffix.get((maxp[1], suf_b), [])
-                
+                for i, j, k1 in position[maxp]:
+                    s = ufs[(i, j)]
+                    v = value[(i, j)]
+                    k2 = k1 + s.getSize(k1)
+                    sizek2 = s.getSize(k2)
+
+                    s.union(k1, k2)
+                    v[k1] = bMaxPair
+                    
+                    if s.has(k1 - 1):
+                        k0 = s.find(k1 - 1)
+                        bPre = v[k0]
+                        increaseD(appearances, (bPre, bMaxPair))
+                        appendD(position, (bPre, bMaxPair), (i, j, k0))
+                        
+                        decreaseD(appearances, (bPre, maxp[0]))
+                        removeD(position, (bPre, maxp[0]), (i, j, k0))
+
+                    if s.has(k2 + sizek2):
+                        k3 = k2 + sizek2
+                        bSuf = v[k3]
+                        increaseD(appearances, (bMaxPair, bSuf))
+                        appendD(position, (bMaxPair, bSuf), (i, j, k1))
+
+                        decreaseD(appearances, (maxp[1], bSuf))
+                        removeD(position, (maxp[1], bSuf), (i, j, k2))
+                        
+                appearances.pop(maxp)
+                position.pop(maxp)
+                v.pop(k2)
+
                 maxn = 0
                 maxp = None
                 for pair in appearances:
-                    if appearances[pair] >= maxn:
+                    if appearances[pair] > maxn or (appearances[pair] == maxn and pair > maxp):
                         maxn = appearances[pair]
                         maxp = pair
 
@@ -113,14 +136,20 @@ def train_bpe(
     vocab_size: int,
     special_tokens: list[str],
 ):
+    from time import time
     tokenizer = BPETokenizer(corpus = input_path, special_tokens=special_tokens)
+    s = time()
     tokenizer.pre_tokenize_corpus()
+    d = time()
+    print(f"Pre-tokenization took {d - s:.2f} seconds.")
     tokenizer.train_bpe(maximum_vocab_size=vocab_size)
-    return tokenizer.vocab, tokenizer.merges
+    print(f"BPE training took {time() - d:.2f} seconds.")
+    reversed_vocab = {v: k for k, v in tokenizer.vocab.items()}
+    return reversed_vocab, tokenizer.merges
 
 if __name__ == "__main__":
     tokenizer = BPETokenizer("data/baby_data.txt", special_tokens=["<|endoftext|>"])
     tokenizer.pre_tokenize_corpus()
     tokenizer.train_bpe(maximum_vocab_size=300)
-    pprint(tokenizer.vocab)
+    # pprint(tokenizer.vocab)
     
---
+++ cs336_basics/utils.py
@@ -0,0 +1,45 @@
+class UnionFindSet:
+    def __init__(self, size: int = 0):
+        self.parent = [x for x in range(size)]
+        self.size = [1] * size
+
+    def find(self, x: int) -> int:
+        # 路径压缩
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x: int, y: int) -> None:
+        root_x = self.find(x)
+        root_y = self.find(y)
+
+        if root_x == root_y:
+            return False
+
+        self.parent[root_y] = root_x
+        self.size[root_x] += self.size[root_y]
+
+        return True
+
+    def getSize(self, x: int) -> int:
+        root = self.find(x)
+        return self.size[root]
+    
+    def has(self, x: int) -> bool:
+        return x >= 0 and x < len(self.parent)
+
+def increaseD(d: dict, key: any):
+    d[key] = d.get(key, 0) + 1
+
+def decreaseD(d: dict, key: any):
+    assert key in d, "Key must exist in the dictionary to decrease its value."
+    d[key] -= 1
+
+def appendD(d: dict, key: any, value: any):
+    if key not in d:
+        d[key] = []
+    d[key].append(value)
+
+def removeD(d: dict, key: any, value: any):
+    d[key].remove(value)
+    
