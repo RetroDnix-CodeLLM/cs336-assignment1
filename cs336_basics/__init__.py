13834120e8c04dcbea1754ad1d4f5fad2e35246c
RetroDnix <retrodnix@gmail.com>
2026-01-19 17:00:29 +0800

    fix: update __init__.py

--- cs336_basics/__init__.py
+++ cs336_basics/__init__.py
@@ -1,3 +1,6 @@
 import importlib.metadata
 
 __version__ = importlib.metadata.version("cs336_basics")
+
+from .model import TransformerLM, MultiHeadSelfAttention, scaledDotProductAttention
+from .optimize import AdamW, cross_entropy_loss, lr_scheduler, gradient_clipping
